e6e018100edcfcf59335ea8897b47ac32f4b54d6
Sean <millington.sean12@gmail.com>
2022-10-19 18:02:30 +0200

    working database

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -3,7 +3,5 @@ const router = express.Router();
 const postController = require('../controllers/posts')
 
 router.get('/', postController.index)
-router.get('/:pseudonym', postController.findByName)
-router.post('/', postController.create)
 
 module.exports = router;
