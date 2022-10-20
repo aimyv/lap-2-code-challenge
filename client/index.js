392051412999226c4508d10625334d289c85b618
Sean <millington.sean12@gmail.com>
2022-10-20 13:49:01 +0200

    register and login

--- client/index.js
+++ client/index.js
@@ -2,11 +2,21 @@ const loginForm = document.getElementById('loginForm');
 loginForm.addEventListener('submit', handleLogin);
 
 const registerForm = document.getElementById('registerForm');
-registerForm.addEventListener('submit', handleRegistration);
+registerForm.addEventListener('submit', handleRegister);
 
 async function handleLogin(e) {
     e.preventDefault()
-    const email = e.target.email.value
-    const password = e.target.password.value
+    const email = e.target.lemail.value
+    const password = e.target.lpassword.value
     console.log(email, password)
+}
+
+async function handleRegister(e) {
+    e.preventDefault()
+    // const data = e.target
+    const email = e.target.remail.value
+    const username = e.target.rusername.value
+    const password = e.target.rpassword.value
+    // console.log(data)
+    console.log(email, password, username)
 }
